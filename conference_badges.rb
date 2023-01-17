9ca46825850c6a8651892ab97126e833f0209eb0
Yuferril <yuferril.yunos@gmail.com>
2023-01-17 20:00:50 +1100

    commit tree 8c7f4783db69e8c5c0342c6d6f7813ddd1ab1de0

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,25 @@
 # Write your code here.
+def badge_maker(name)
+     "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+    attendees.map do |name|
+        badge_maker(name)
+    end 
+end
+
+def assign_rooms(attendees)
+    attendees.map.with_index(1) do |name, room|
+         "Hello, #{name}! You'll be assigned to room #{room}!"
+    end
+end
+
+def printer(attendees)
+    batch_badge_creator(attendees).each do |name|
+        puts name
+    end
+    assign_rooms(attendees).each do |name|
+        puts name
+    end
+end
